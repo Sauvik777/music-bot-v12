0e85fa24667140e37e720b63a9c28ce37e5e2830
Sauvik777 <165468193+Sauvik777@users.noreply.github.com>
2024-10-18 14:21:50 +0530

    Update error.js

--- player-events/error.js
+++ player-events/error.js
@@ -1,17 +1,22 @@
 module.exports = (client, error, message) => {
+    console.error(`Music error: ${error}`);
 
-    switch (error) {
-        case 'NotPlaying':
-            message.channel.send(`${client.emotes.error} - There is no music being played on this server !`);
-            break;
-        case 'NotConnected':
-            message.channel.send(`${client.emotes.error} - You are not connected in any voice channel !`);
-            break;
-        case 'UnableToJoin':
-            message.channel.send(`${client.emotes.error} - I am not able to join your voice channel, please check my permissions !`);
-            break;
-        default:
-            message.channel.send(`${client.emotes.error} - Something went wrong ... Error : ${error}`);
-    };
-
+    if (message) {
+        switch (error) {
+            case 'NotPlaying':
+                message.channel.send(`${client.emotes.error} - There is no music being played on this server!`);
+                break;
+            case 'NotConnected':
+                message.channel.send(`${client.emotes.error} - You are not connected to any voice channel!`);
+                break;
+            case 'UnableToJoin':
+                message.channel.send(`${client.emotes.error} - I am unable to join your voice channel. Please check my permissions!`);
+                break;
+            default:
+                message.channel.send(`${client.emotes.error} - Something went wrong... Error: ${error}`);
+        }
+    } else {
+        console.error(`Error occurred but no message context available: ${error}`);
+    }
 };
+
